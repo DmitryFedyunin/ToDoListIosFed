d2f6a529c481f77b6e3a87370221db0b2986f807
дима мак <dimamak@imac-dima.local>
2019-03-24 17:21:41 +0600

    Initial Commit

---
+++ ToDoListIosFed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListIosFed
+//
+//  Created by дима мак on 24.03.2019.
+//  Copyright © 2019 дима мак. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
